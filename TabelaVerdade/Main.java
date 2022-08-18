17a83627f2d306f31857a993797f18ab2db2f29a
Lucas de Jesus <lucaskreutzerdejesus@gmail.com>
2022-08-18 17:38:36 -0300

    Update Main.java

--- TabelaVerdade/Main.java
+++ TabelaVerdade/Main.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Instanciando a classe que receberá a expressão do usuário e a classe das funcionalidades da tabela.
+        Scanner input = new Scanner(System.in);
+        TabelaVerdade tabela = new TabelaVerdade();
+
+        // Criando um loop apenas para garantir que o usuário possa continuar o programa caso digite algo errado.
+        while (true) {
+
+            // Pedindo para o usuário digitar a expressão digitada, seguindo as limitações.
+            System.out.println("Digite a expressão lógica, utilizando variáveis p, q, r, e canônicos (~/^/v), ou S para encerrar: ");
+
+            // Armazenando a expressão digitada em uma variável.
+            String expressao = input.nextLine();
+
+            // Usuário tecla S/s caso queira encerar o programa.
+            if(expressao.equals("s") || expressao.equals("S")) {
+                System.exit(0);
+            }
+            else {
+                // Enviando a expressão para o método.
+                tabela.checkVariaveis(expressao);
+            }
+        }
+    }
+}
